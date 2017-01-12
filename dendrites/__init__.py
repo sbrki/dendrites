0af9eb97e2ae65519e11a94e4aedbd163f91ec6d
Kevin Schellenberg <wikkiewikkie@gmail.com>
2017-01-12 13:51:53 -0500

    add setup

--- dendrites/__init__.py
+++ dendrites/__init__.py
@@ -1,3 +1,5 @@
 from .neural_network import NeuralNetwork
 
+__version__ = "0.1.0"
+
 __all__ = ["NeuralNetwork"]
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+import dendrites
+
+setup(
+    name='dendrites',
+    version=dendrites.__version__,
+    packages=[
+        'dendrites'
+    ],
+    platforms='any',
+    zip_safe=True,
+    install_requires=[
+        'numpy'
+    ],
+    url='https://github.com/whiteShtef/dendrites',
+    license="Apache 2.0",
+    author="Stjepan",
+    author_email="whiteShtef@users.noreply.github.com",
+    description='Neural networks for humans',
+    long_description="Dendrites aims to be a straightforward and very "
+                     "easy to use neural network tool for Python3.",
+    keywords="",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ]
+)
